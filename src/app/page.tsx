88b32844c6fcc9663e053f01e76a66ada4f6c401
Nick Zhou <39304873+nickchow2020@users.noreply.github.com>
2025-04-26 13:46:55 -0500

    add layout for information and instruction and notice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,8 +48,8 @@ export default function Page() {
                 </div>
             </section>
             <section>
-                <div className="flex flex-col mt-3">
-                    <h2 className="text-center font-extrabold">
+                <div className="flex flex-col my-3">
+                    <h2 className="text-center font-extrabold text-[17px] mt-2">
                         设备维护报价单
                     </h2>
                     <div className="flex justify-between">
@@ -107,6 +107,39 @@ export default function Page() {
                             />
                         </div>
                     </div>
+                </div>
+            </section>
+            <section>
+                <h2 className="font-extrabold text-[17px]">故障说明:</h2>
+            </section>
+            <section>
+                <h2 className="font-extrabold text-[17px]">报价说明:</h2>
+            </section>
+            <section>
+                <h4 className=" text-[13px]">注意事项：</h4>
+                <div className="flex justify-start">
+                    <ul className="ml-5 mr-30">
+                        <li className="text-[13px] mb-1">
+                            1.付款条件 : 100% 预付
+                        </li>
+                        <li className="text-[13px] mb-1">
+                            2.维护时间 : 确认维修后7个工作日
+                        </li>
+                        <li className="text-[13px] mb-1">
+                            3.维护保固期 : 3个月(非人为损坏)
+                        </li>
+                        <li className="text-[13px] mb-1">
+                            4.该报价单位为企业商业机密，未经允许禁止对外传播
+                        </li>
+                        <li className="text-[13px]">
+                            5.如确认维护请将报价单签字盖章回传
+                        </li>
+                    </ul>
+                    <ul className="mt-3">
+                        <li className="text-[13px] mb-1">客户确认 :</li>
+                        <li className="text-[13px] mb-1">(签字盖章)</li>
+                        <li className="text-[13px]">确认日期 :</li>
+                    </ul>
                 </div>
             </section>
         </main>
