9e5040fa0b8bbc4adc8e0978f279a0b4b9bd50de
Nick Zhou <39304873+nickchow2020@users.noreply.github.com>
2025-04-26 11:38:43 -0500

    set up next js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "你老公来了",
+    description: "给我老婆的一个小网站",
+};
+
 export default function RootLayout({
     children,
 }: {
