1faf7d2592c0fefbdf58904f15aebf470a9fe897
Nick Zhou <39304873+nickchow2020@users.noreply.github.com>
2025-04-26 13:17:34 -0500

    add title and maintain devices section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,114 @@
+import { cn } from "utility/helper";
+
+function ColumnDisplay({
+    label,
+    value,
+    className,
+    labelClassName,
+    valueClassName,
+}: {
+    label: string;
+    value: string;
+    className?: string;
+    labelClassName?: string;
+    valueClassName?: string;
+}) {
+    return (
+        <div className={cn("flex text-blue-800", className)}>
+            <div className={labelClassName}>{label}</div>
+            <span className="font-medium">:</span>
+            <div className={valueClassName}>{value}</div>
+        </div>
+    );
+}
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>;
+    return (
+        <main className="w-[210mm]">
+            <section className="flex flex-col items-center justify-center">
+                <h1 className="font-extrabold text-2xl mb-1">
+                    苏州慕翰电子科技有限公司
+                </h1>
+                <div className="flex border-b-1 border-b-blue-800 text-[13px]">
+                    <ColumnDisplay
+                        label="联系人"
+                        value="宗培芳"
+                        className="mr-3"
+                    />
+                    <ColumnDisplay
+                        label="Tel"
+                        value="18676737950"
+                        className="mr-3"
+                    />
+
+                    <ColumnDisplay
+                        label="Email"
+                        value="peifang.zong@esamber.com"
+                    />
+                </div>
+            </section>
+            <section>
+                <div className="flex flex-col mt-3">
+                    <h2 className="text-center font-extrabold">
+                        设备维护报价单
+                    </h2>
+                    <div className="flex justify-between">
+                        <div>
+                            <ColumnDisplay
+                                label="报价单号"
+                                value="2023100901"
+                                className="text-black mb-1"
+                                labelClassName="w-[70px]"
+                                valueClassName="ml-3"
+                            />
+                            <ColumnDisplay
+                                label="客户"
+                                value="peifang.zong@esamber.com"
+                                className="text-black mb-1"
+                                labelClassName="w-[70px]"
+                                valueClassName="ml-3"
+                            />
+                            <ColumnDisplay
+                                label="联系人"
+                                value="peifang.zong@esamber.com"
+                                className="text-black mb-1"
+                                labelClassName="w-[70px]"
+                                valueClassName="ml-3"
+                            />
+                            <ColumnDisplay
+                                label="电话"
+                                value="peifang.zong@esamber.com"
+                                className="text-black"
+                                labelClassName="w-[70px]"
+                                valueClassName="ml-3"
+                            />
+                        </div>
+                        <div>
+                            <ColumnDisplay
+                                label="日期"
+                                value="peifang.zong@esamber.com"
+                                className="text-black mb-1"
+                                labelClassName="w-[40px]"
+                                valueClassName="ml-3"
+                            />
+                            <ColumnDisplay
+                                label="业务"
+                                value="peifang.zong@esamber.com"
+                                className="text-black mb-1"
+                                labelClassName="w-[40px]"
+                                valueClassName="ml-3"
+                            />
+                            <ColumnDisplay
+                                label="币别"
+                                value="peifang.zong@esamber.com"
+                                className="text-black"
+                                labelClassName="w-[40px]"
+                                valueClassName="ml-3"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
 }
