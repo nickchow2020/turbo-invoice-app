942a905d4a9cdb5ad40a813bf56b8417fcb8bc3e
Nick Zhou <39304873+nickchow2020@users.noreply.github.com>
2025-04-27 18:52:11 -0500

    update page width column

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,95 +31,105 @@ export default function Page() {
                     <Footer />
                 </section>
             </div>
-            <div className="px-10 py-4 border-l-2 border-l-blue-800 min-w-[210mm] flex">
-                <section className="pr-10">
-                    <h2 className="font-extrabold text-[17px] mb-4">
-                        基本信息:
-                    </h2>
-                    <InputColumn
-                        title="报价单号"
-                        placeholder="输入报价单号"
-                        inputClassName="w-70"
-                    />
-                    <InputColumn
-                        title="联系人"
-                        defaultValue="宗培芳"
-                        placeholder="输入联系人姓名"
-                        inputClassName="w-70"
-                    />
-                    <InputColumn
-                        title="姓名"
-                        defaultValue="宗培芳"
-                        placeholder="输入报价人姓名"
-                        inputClassName="w-70"
-                    />
-                    <InputColumn
-                        title="邮箱"
-                        defaultValue="peifang.zong@esamber.com"
-                        placeholder="输入联系人电话"
-                        inputType="email"
-                        inputClassName="w-70"
-                    />
-                    <InputColumn
-                        title="电话"
-                        defaultValue="18676737950"
-                        placeholder="输入联系人电话"
-                        inputClassName="w-70"
-                    />
-                    <SelectColumn
-                        title="业务"
-                        placeholder="请选择服务业务"
-                        dropdownItems={serviceItems}
-                        selectorClassName="w-70"
-                    />
-                    <SelectColumn
-                        title="币别"
-                        placeholder="请选择币别"
-                        dropdownItems={currencyItems}
-                        selectorClassName="w-70"
-                    />
+            <div className="px-10 py-4 border-l-2 border-l-blue-800 min-w-[210mm]">
+                <article className="flex h-fit w-full">
+                    <section className="pr-10">
+                        <h2 className="font-extrabold text-[17px] mb-4">
+                            基本信息:
+                        </h2>
+                        <InputColumn
+                            title="报价单号"
+                            placeholder="输入报价单号"
+                            inputClassName="w-70"
+                        />
+                        <InputColumn
+                            title="联系人"
+                            defaultValue="宗培芳"
+                            placeholder="输入联系人姓名"
+                            inputClassName="w-70"
+                        />
+                        <InputColumn
+                            title="姓名"
+                            defaultValue="宗培芳"
+                            placeholder="输入报价人姓名"
+                            inputClassName="w-70"
+                        />
+                        <InputColumn
+                            title="邮箱"
+                            defaultValue="peifang.zong@esamber.com"
+                            placeholder="输入联系人电话"
+                            inputType="email"
+                            inputClassName="w-70"
+                        />
+                        <InputColumn
+                            title="电话"
+                            defaultValue="18676737950"
+                            placeholder="输入联系人电话"
+                            inputClassName="w-70"
+                        />
+                        <SelectColumn
+                            title="业务"
+                            placeholder="请选择服务业务"
+                            dropdownItems={serviceItems}
+                            selectorClassName="w-70"
+                        />
+                        <SelectColumn
+                            title="币别"
+                            placeholder="请选择币别"
+                            dropdownItems={currencyItems}
+                            selectorClassName="w-70"
+                        />
 
-                    <CalenderColumn title="日期" placeholder={"请选择日期"} />
-                </section>
-                <section className="pl-10">
+                        <CalenderColumn
+                            title="日期"
+                            placeholder={"请选择日期"}
+                        />
+                    </section>
+                    <section>
+                        <h2 className="font-extrabold text-[17px] mb-4">
+                            故障说明:
+                        </h2>
+                        <InputColumn
+                            title="ID号"
+                            placeholder="请输入ID号"
+                            inputClassName="w-100"
+                        />
+                        <InputColumn
+                            title="需求单位"
+                            placeholder="请输入需求单位"
+                            inputClassName="w-100"
+                        />
+                        <InputColumn
+                            title="设备名称"
+                            placeholder="请输入设备名称"
+                            inputClassName="w-100"
+                        />
+                        <InputColumn
+                            title="设备型号"
+                            placeholder="请输入设备型号"
+                            inputClassName="w-100"
+                        />
+                        <InputColumn
+                            title="用户反馈"
+                            placeholder="请填写用户反馈内容"
+                            inputClassName="w-100"
+                        />
+                        <TextareaColumn
+                            title="送修检测"
+                            placeholder="请填写送修检测内容"
+                        />
+                        <CalenderColumn
+                            title="出货时间"
+                            placeholder={"请选择出货日期"}
+                            selectorClassName="w-100"
+                        />
+                    </section>
+                </article>
+                <article>
                     <h2 className="font-extrabold text-[17px] mb-4">
-                        故障说明:
+                        报价说明:
                     </h2>
-                    <InputColumn
-                        title="ID号"
-                        placeholder="请输入ID号"
-                        inputClassName="w-100"
-                    />
-                    <InputColumn
-                        title="需求单位"
-                        placeholder="请输入需求单位"
-                        inputClassName="w-100"
-                    />
-                    <InputColumn
-                        title="设备名称"
-                        placeholder="请输入设备名称"
-                        inputClassName="w-100"
-                    />
-                    <InputColumn
-                        title="设备型号"
-                        placeholder="请输入设备型号"
-                        inputClassName="w-100"
-                    />
-                    <InputColumn
-                        title="用户反馈"
-                        placeholder="请填写用户反馈内容"
-                        inputClassName="w-100"
-                    />
-                    <TextareaColumn
-                        title="送修检测"
-                        placeholder="请填写送修检测内容"
-                    />
-                    <CalenderColumn
-                        title="出货时间"
-                        placeholder={"请选择出货日期"}
-                        selectorClassName="w-100"
-                    />
-                </section>
+                </article>
             </div>
         </main>
     );
