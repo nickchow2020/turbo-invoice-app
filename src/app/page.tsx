3911fe20172a76c5556a20fb1d365cd04cf61af1
Nick Zhou <39304873+nickchow2020@users.noreply.github.com>
2025-05-04 10:13:24 -0500

    add useForm actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import BasicInfo from "@/components/BasicInfo";
 import Footer from "@/components/Footer";
 import Instruction from "@/components/Instruction";
@@ -6,38 +7,45 @@ import MaintenanceInstructionTable from "@/components/MaintenanceInstructionTabl
 import PersonalInfo from "@/components/PersonalInfo";
 import PricingTable from "@/components/PricingTable";
 import { TitleInfo } from "@/components/TitleInfo";
-
+import { FormProvider, useForm } from "react-hook-form";
 export default function Page() {
+    const methods = useForm();
     return (
-        <main className="flex">
-            <div className="w-[210mm] px-6 py-4">
-                <section className="flex flex-col items-center justify-center">
-                    <TitleInfo />
-                </section>
-                <section>
-                    <PersonalInfo />
-                </section>
-                <section className="mb-3">
-                    <h2 className="font-extrabold text-[17px]">故障说明:</h2>
-                    <MaintenanceInstructionTable />
-                </section>
-                <section className="mb-5">
-                    <h2 className="font-extrabold text-[17px]">报价说明:</h2>
-                    <PricingTable />
-                </section>
-                <section>
-                    <Footer />
-                </section>
-            </div>
-            <div className="px-10 py-4 border-l-2 border-l-blue-800 min-w-[210mm]">
-                <article className="flex h-fit w-full">
-                    <BasicInfo />
-                    <Instruction />
-                </article>
-                <article>
-                    <InvoiceDetail />
-                </article>
-            </div>
-        </main>
+        <FormProvider {...methods}>
+            <main className="flex">
+                <div className="w-[210mm] px-6 py-4">
+                    <section className="flex flex-col items-center justify-center">
+                        <TitleInfo />
+                    </section>
+                    <section>
+                        <PersonalInfo />
+                    </section>
+                    <section className="mb-3">
+                        <h2 className="font-extrabold text-[17px]">
+                            故障说明:
+                        </h2>
+                        <MaintenanceInstructionTable />
+                    </section>
+                    <section className="mb-5">
+                        <h2 className="font-extrabold text-[17px]">
+                            报价说明:
+                        </h2>
+                        <PricingTable />
+                    </section>
+                    <section>
+                        <Footer />
+                    </section>
+                </div>
+                <div className="px-10 py-4 border-l-2 border-l-blue-800 min-w-[210mm]">
+                    <article className="flex h-fit w-full">
+                        <BasicInfo />
+                        <Instruction />
+                    </article>
+                    <article>
+                        <InvoiceDetail />
+                    </article>
+                </div>
+            </main>
+        </FormProvider>
     );
 }
