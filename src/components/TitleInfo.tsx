ff5ac8e159eecc2a49b5f4fd916baded7791800d
Nick Zhou <39304873+nickchow2020@users.noreply.github.com>
2025-05-04 18:58:27 -0500

    update title info

--- src/components/TitleInfo.tsx
+++ src/components/TitleInfo.tsx
@@ -1,20 +1,28 @@
+"use client";
+import { FormValues } from "app/page";
 import { ColumnDisplay } from "./common/ColumnDisplay";
+import { useFormContext, useWatch } from "react-hook-form";
 
 export function TitleInfo() {
+    const { control } = useFormContext<FormValues>();
+    const { contactName, email, phone } = useWatch({
+        control,
+    });
+
     return (
         <>
             <h1 className="font-extrabold text-2xl mb-1">
                 苏州慕翰电子科技有限公司
             </h1>
             <div className="flex border-b-1 border-b-blue-800 text-[13px]">
-                <ColumnDisplay label="联系人" value="宗培芳" className="mr-3" />
                 <ColumnDisplay
-                    label="Tel"
-                    value="18676737950"
+                    label="联系人"
+                    value={contactName}
                     className="mr-3"
                 />
+                <ColumnDisplay label="Tel" value={phone} className="mr-3" />
 
-                <ColumnDisplay label="Email" value="peifang.zong@esamber.com" />
+                <ColumnDisplay label="Email" value={email} />
             </div>
         </>
     );
